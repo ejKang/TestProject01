84c5c9efcc4c35bf7f224f100ace647cafb3b03f
eunjoo_kang <eunjoo_kang@tmax.co.kr>
2020-03-16 19:11:38 +0900

    add jeus file

---
+++ dockerRsc/install/proobject7/_for_jeus/settingDev.js
@@ -0,0 +1,16 @@
+var poDevSrvInfo = Top.Data.create({
+      ip: "%SERVER_IP%",
+      port: "%HTTP_PORT%",
+      App: "/proobject",
+      SG: "/proobject-manager",
+      Service: ""
+});
+
+var testSrvInfo =  Top.Data.create({
+      ip: "%SERVER_IP%",
+      port: "%HTTP_PORT%",
+      App: "/proobject",
+      SG: "/proobject-testserver",
+      Service: ""
+});
+
---
+++ dockerRsc/install/proobject7/_for_jeus/settingOps.js
@@ -0,0 +1,7 @@
+var poOperSrvInfo = Top.Data.create({
+  ip: "%SERVER_IP%",      //운영계 ProManager를 배포한 PO7 서버 ip (MASTER NODE)
+  port: "%HTTP_PORT%",             //운영계 ProManager를 배포한 PO7 서버 http port (MASTER NODE)
+  App: "/proobject",
+  SG: "/proobject-managerops",
+  Service: ""
+});
